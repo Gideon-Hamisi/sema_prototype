7db213dd571ab7e36e75535883ae1d9558085f76
Neocipher Technologies <neociphertechnologies @gmail.com>
2025-03-26 21:03:02 +0300

    Deploy web app

--- app/server.py
+++ app/server.py
@@ -162,5 +162,5 @@ def index():
 
 if __name__ == "__main__":
     init_db()
-    port = int(os.environ.get("PORT", 5000))  # Heroku sets PORT
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT
     app.run(debug=False, host='0.0.0.0', port=port)
